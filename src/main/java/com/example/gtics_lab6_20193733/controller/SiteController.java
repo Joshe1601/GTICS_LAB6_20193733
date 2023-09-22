4dc6fe24ae8b4c2b08f9067c6abeaa9c66c744c5
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-09-21 21:37:15 -0500

    Se realizó la impleentación de la vista de nuevo ticket y con ella la recepción Post

--- src/main/java/com/example/gtics_lab6_20193733/controller/SiteController.java
+++ src/main/java/com/example/gtics_lab6_20193733/controller/SiteController.java
@@ -1,11 +1,14 @@
 package com.example.gtics_lab6_20193733.controller;
 
+import com.example.gtics_lab6_20193733.entity.Location;
 import com.example.gtics_lab6_20193733.entity.Site;
 import com.example.gtics_lab6_20193733.repository.SiteRepository;
+import com.example.gtics_lab6_20193733.repository.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -14,6 +17,9 @@ public class SiteController {
 
     @Autowired
     private SiteRepository siteRepository;
+
+    @Autowired
+    private LocationRepository locationRepository;
 
 
     @GetMapping(value ="/")
@@ -29,8 +35,18 @@ public class SiteController {
     }
 
     @GetMapping(value ="/sitios/nuevo")
-    public String nuevoSitio() {
+    public String nuevoSitio(Model model) {
+
+        List<Location> listaLocations = locationRepository.findAll();
+        model.addAttribute("listaLocations", listaLocations);
         return "nuevo-sitio";
+    }
+
+    @GetMapping(value ="/sitios/guardar")
+    public String guardarSitio(Model model, Site sitio, RedirectAttributes attr) {
+        siteRepository.save(sitio);
+        attr.addFlashAttribute("msg", "Sitio" + sitio.getSitename() + "creado exitosamente");
+        return "redirect:/sitios";
     }
 
 
