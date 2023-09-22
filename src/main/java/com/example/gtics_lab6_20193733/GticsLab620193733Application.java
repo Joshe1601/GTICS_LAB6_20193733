f59f244f9254814b20675f87aed6b1048114c8de
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-09-21 20:25:57 -0500

    Empezamos el laboratorio

---
+++ src/main/java/com/example/gtics_lab6_20193733/GticsLab620193733Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab6_20193733;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab620193733Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab620193733Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtics_lab6_20193733/GticsLab620193733ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab6_20193733;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab620193733ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
