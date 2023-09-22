f9e21bd579e19810f0597e52d4140fdd4646b996
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-09-21 20:53:36 -0500

    Se creó la página principal donde se mostrarán los sitios y se usó fragmentos para poder 'importar' el header estático, además se creó el controlador del sitio

--- src/main/java/com/example/gtics_lab6_20193733/controller/SiteController.java
+++ src/main/java/com/example/gtics_lab6_20193733/controller/SiteController.java
@@ -1,4 +1,14 @@
 package com.example.gtics_lab6_20193733.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class SiteController {
+
+    @GetMapping(value = {"/", "/lista-sitios"})
+    public String principalSitios() {
+        return "lista-sitios";
+    }
+
 }
