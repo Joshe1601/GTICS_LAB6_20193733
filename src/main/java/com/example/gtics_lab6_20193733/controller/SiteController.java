19a88ae35cbabbd2005a33d0ee90896bf46f209c
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-09-21 21:01:32 -0500

    Se separó la vista inicial de la vista de lista de sitios

--- src/main/java/com/example/gtics_lab6_20193733/controller/SiteController.java
+++ src/main/java/com/example/gtics_lab6_20193733/controller/SiteController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SiteController {
 
-    @GetMapping(value = {"/", "/lista-sitios"})
+    @GetMapping(value ="/")
+    public String defaultPage() {
+        return "index";
+    }
+
+    @GetMapping(value ="/sitios")
     public String principalSitios() {
         return "lista-sitios";
     }
+
+    
 
 }
