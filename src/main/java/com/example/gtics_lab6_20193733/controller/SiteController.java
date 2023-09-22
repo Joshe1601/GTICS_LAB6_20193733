d6575d067a07d1c40e5ae956522982a78b17cf5e
Jose Morillos <jmorillosp@pucp.edu.pe>
2023-09-21 21:43:45 -0500

    Guardar y borrar correctamente implementados

--- src/main/java/com/example/gtics_lab6_20193733/controller/SiteController.java
+++ src/main/java/com/example/gtics_lab6_20193733/controller/SiteController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -42,12 +44,21 @@ public class SiteController {
         return "nuevo-sitio";
     }
 
-    @GetMapping(value ="/sitios/guardar")
-    public String guardarSitio(Model model, Site sitio, RedirectAttributes attr) {
-        siteRepository.save(sitio);
-        attr.addFlashAttribute("msg", "Sitio" + sitio.getSitename() + "creado exitosamente");
+    @GetMapping(value ="/sitios/borrar/{id}")
+    public String borrarSitio(Model model, @PathVariable Integer id, RedirectAttributes attr) {
+        attr.addFlashAttribute("msg2", "Sitio " + siteRepository.findById(id) + " borrado exitosamente");
+        siteRepository.deleteById(id);
         return "redirect:/sitios";
     }
+
+    @PostMapping(value ="/sitios/guardar")
+    public String guardarSitio(Model model, Site sitio, RedirectAttributes attr) {
+        siteRepository.save(sitio);
+        attr.addFlashAttribute("msg", "Sitio " + sitio.getSitename() + " creado exitosamente");
+        return "redirect:/sitios";
+    }
+
+
 
 
 
